3ec7234a3904c095be403c5a38d4005ff1af8c47
Tvsmadhur <67414114+Tvsmadhur@users.noreply.github.com>
2021-05-09 19:05:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
+const name=["madhur","james","games"];
+  const listitem=name.map((names)=>
+  <li key={names}>{names}</li>
+  );
         return(
             <div id="main">
                {/* Do not remove the main div */}
+             <ul>{listitem}</ul>
             </div>
         )
     }
